599c4d9bd7d8e6944cc9bfac50cc2b01321efa82
alexpre <33905114+alexpre@users.noreply.github.com>
2018-03-24 14:58:00 +0200

    ComboBox Update

--- Creatie.java
+++ Creatie.java
@@ -2,12 +2,32 @@ import javax.swing.*;
 
 public class Creatie{
 
+    public static void CreatieButon(JButton btn, String text, int x, int y,int width,int height) {
 
-    public static void CreatieButon(JButton btn, String text, int x, int y) {
-
-        btn.setBounds(x,y,120,35);
+        btn.setBounds(x,y,width,height);
         btn.setText(text);
     }
 
+    public static void CreatiePanel(JPanel pan,int x,int y){
 
+        pan.setSize(x,y);
+        pan.setLayout(null);
+    }
+
+    public static void CreatieFrame(JFrame frame,int x,int y){
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(x,y);
+    }
+
+    public static void CreatieBox(JComboBox box,boolean stat,boolean editable,int x,int y,int width,int height){
+
+        box.setVisible(stat);
+        box.setBounds(x,y,width,height);
+        if(editable == true)
+        {
+            box.isEditable();
+        }
+    }
 }
