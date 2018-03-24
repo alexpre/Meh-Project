49f61fc3670dfc461f655ee6661e47dd10bf7cd8
Zebranebuna <37732902+Zebranebuna@users.noreply.github.com>
2018-03-24 13:23:44 +0200

    a
    a

--- Creatie.java
+++ Creatie.java
@@ -3,11 +3,16 @@ import javax.swing.*;
 public class Creatie{
 
 
-    public static void CreatieButon(JButton btn, String text, int x, int y) {
+    public static void CreatieButon(JButton btn, String text, int x, int y,int width,int height) {
 
-        btn.setBounds(x,y,120,35);
+        btn.setBounds(x,y,width,height);
         btn.setText(text);
     }
 
+    public static void CreatiePanel(JPanel pan,int x,int y){
+
+        pan.setSize(x,y);
+        pan.setLayout(null);
+    }
 
 }
--- Main.java
+++ Main.java
@@ -10,16 +10,15 @@ public class Main {
     public static void gui(){
 
         JPanel pan = new JPanel();
-        pan.setSize(500, 500);
+        Creatie.CreatiePanel(pan,500,500);
         JButton btn = new JButton();
-        Creatie.CreatieButon(btn,"Text",100,100);
+        Creatie.CreatieButon(btn,"Text",100,100,120,35);
         JButton btn2 = new JButton();
-        Creatie.CreatieButon(btn2,"Text2",200,200);
+        Creatie.CreatieButon(btn2,"Text2",200,200,120,35);
         JFrame frame=new JFrame();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(510,510);
-        pan.setLayout(null);
         pan.add(btn);
         pan.add(btn2);
         frame.add(pan);
