ff4eb2a28341bccca68b9871f00d844a3f7c1b83
alexpre <33905114+alexpre@users.noreply.github.com>
2018-03-24 17:16:13 +0200

    Almost Final

--- FronPage.java
+++ FronPage.java
@@ -16,12 +16,21 @@ public class FronPage {
         JButton vanzari = new JButton();
         Creatie.CreatieButon(vanzari,"Vanzari",15,639,120,30);
 
+        JButton undo = new JButton();
+        Creatie.CreatieButon(undo,"Undo",740,220,50,50);
+
+        JPanel logo = new JPanel();
+        Creatie.CreatiePanel(logo,0,0,250,150);
+        logo.setBorder(BorderFactory.createLineBorder(Color.black));
+
         JPanel all = new JPanel();
         Creatie.CreatiePanel(all,0,0,920,720);
 
         all.add(comenzi);
         all.add(logareAdmin);
         all.add(vanzari);
+        all.add(undo);
+        all.add(logo);
 
         JFrame frame = new JFrame();
         Creatie.CreatieFrame(frame,920,720);
