cb4ec25e367767130bc2169a548fd94dc9e0495c
alexpre <33905114+alexpre@users.noreply.github.com>
2018-03-31 18:23:31 +0300

    Well

--- FronPage.java
+++ FronPage.java
@@ -2,9 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.sql.Array;
+import java.util.Arrays;
 
 
-public class FronPage implements Runnable {
+public class FronPage {
+
+    private static String id = "admin";
+    private static String password1 = "admin";
+    private static JButton logOut = new JButton();
+    private static JButton adaugaPizza = new JButton();
+    private static Frame loginFrame = new Frame(300,300);
 
     public static void Front()
 
@@ -12,31 +22,31 @@ public class FronPage implements Runnable {
         // Draw the interface //
 
         JLabel comenzi = new JLabel();
-        Creatie.CreatieLabel(comenzi,180,220,560,450);
+        Creatie.CreatieLabel(comenzi, 180, 220, 560, 450);
 
         JButton logareAdmin = new JButton();
-        Creatie.CreatieButon(logareAdmin,"Admin",770,639,120,30);
+        Creatie.CreatieButon(logareAdmin, "Admin", 770, 639, 120, 30);
 
         JButton vanzari = new JButton();
-        Creatie.CreatieButon(vanzari,"Vanzari",15,639,120,30);
+        Creatie.CreatieButon(vanzari, "Vanzari", 15, 639, 120, 30);
 
         JButton undo = new JButton();
-        Creatie.CreatieButon(undo,"Undo",740,220,50,50);
+        Creatie.CreatieButon(undo, "Undo", 740, 220, 50, 50);
 
         JPanel logo = new JPanel();
-        Creatie.CreatiePanel(logo,0,0,250,150);
+        Creatie.CreatiePanel(logo, 0, 0, 250, 150);
         logo.setBorder(BorderFactory.createLineBorder(Color.black));
 
         JPanel all = new JPanel();
-        Creatie.CreatiePanel(all,0,0,920,720);
+        Creatie.CreatiePanel(all, 0, 0, 920, 720);
 
-        String data[][] ={{"1","Vera","12" + "lei"},
-                         {"2","Not Vera", "21" + "lei"}};
+        String data[][] = {{"1", "Vera", "12" + "lei"},
+                {"2", "Not Vera", "21" + "lei"}};
 
-        String column[] = {"Nr.","Pizza Name","Price"};
+        String column[] = {"Nr.", "Pizza Name", "Price"};
 
-        JTable jt = new JTable(data,column);
-        jt.setBounds(0,0,560,450);
+        JTable jt = new JTable(data, column);
+        jt.setBounds(0, 0, 560, 450);
         comenzi.add(jt);
 
 
@@ -46,31 +56,93 @@ public class FronPage implements Runnable {
         all.add(undo);
         all.add(logo);
 
-        JFrame frame = new JFrame();
+        Frame frame = new Frame(920,720);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        Creatie.CreatieFrame(frame,920,720);
         frame.add(all);
 
-        // Action Listeners //
 
+
+        //********************* ACTION LISTENERS *********************//
+
+                             // Log in panel popUp //
         logareAdmin.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-                LoginPopUp.Login();
+
+                JLabel username = new JLabel("Username:");
+                username.setBounds(20,20,80,30);
+
+                JLabel password = new JLabel("Password:");
+                password.setBounds(20,75,80,30);
+
+                JTextField usernameField = new JTextField();
+                usernameField.setBounds(100,20,100,30);
+
+                JPasswordField passwordField = new JPasswordField();
+                passwordField.setBounds(100,75,100,30);
+
+                JButton login = new JButton();
+                Creatie.CreatieButon(login,"Login",100,130,90,30);
+
+                JPanel allLogin = new JPanel();
+                Creatie.CreatiePanel(allLogin,0,0,300,300);
+
+                allLogin.add(username);
+                allLogin.add(password);
+                allLogin.add(usernameField);
+                allLogin.add(passwordField);
+                allLogin.add(login);
+
+               // Frame loginFrame = new Frame(300,300);
+
+                loginFrame.add(allLogin);
+                loginFrame.setVisible(true);
+
+                // LOG IN BUTTON ACTION //
+                login.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                        char[] password = passwordField.getPassword();
+                        char[] correctPass = new char[] {'a', 'd', 'm', 'i', 'n'};
+
+                        if(usernameField.getText().equals("admin") && Arrays.equals(password,correctPass)) {
+
+                            Creatie.CreatieButon(adaugaPizza, "Adauga Pizza", 15, 500, 120, 30);
+                            Creatie.CreatieButon(logOut, "Deconectare", 15, 400, 120, 30);
+
+                            all.add(logOut);
+                            all.add(adaugaPizza);
+                            frame.revalidate();
+                            frame.repaint();
+                            loginFrame.setVisible(false);
+                            loginFrame.dispose();
+
+                        }else{
+
+                            JOptionPane.showMessageDialog(loginFrame,"ID or Password incorect!");
+
+                        }
+
+                    }
+                });
+
             }
         });
 
+                                // DISCCONECT BUTTON ACTION //
+        logOut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                all.remove(adaugaPizza);
+                all.remove(logOut);
+                frame.revalidate();
+                frame.repaint();
+
+            }
+        });
     }
-
-    @Override
-    public void run() {
-
-    }
-
-    //////////////////////// TEST //////////////////////
-
-
-
-    }
+}
 
 
